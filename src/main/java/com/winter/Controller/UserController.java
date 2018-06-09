2c3da8735cb799c06b7786bc27818fb5a7a8e182
Administrator <Administrator@HW-PC>
2018-06-09 20:44:09 +0800

    修改项目sweagger

--- src/main/java/com/winter/Controller/UserController.java
+++ src/main/java/com/winter/Controller/UserController.java
@@ -2,6 +2,10 @@ package com.winter.Controller;
 
 import com.winter.model.User;
 import com.winter.service.user.UserService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,23 +15,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by Administrator on 2017/8/16.
  */
+@Api("用户信息")
 @Controller
 @RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
-
+    @ApiOperation(value="插入信息", notes="插入用户信息")
     @ResponseBody
     @RequestMapping(value = "/add", produces = {"application/json;charset=UTF-8"})
     public int addUser(User user){
         return userService.addUser(user);
     }
-
+    @ApiOperation(value="查询信息", notes="查询用户信息")
     @ResponseBody
     @RequestMapping(value = "/all/{pageNum}/{pageSize}", produces = {"application/json;charset=UTF-8"})
     public Object findAllUser(@PathVariable("pageNum") int pageNum, @PathVariable("pageSize") int pageSize){
 
         return userService.findAllUser(pageNum,pageSize);
     }
+    
+ 
 }
--- src/main/java/com/winter/SpringbootMybatisDemoApplication.java
+++ src/main/java/com/winter/SpringbootMybatisDemoApplication.java
@@ -1,10 +1,12 @@
 package com.winter;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-		import org.mybatis.spring.annotation.MapperScan;
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @MapperScan("com.winter.mapper")
 public class SpringbootMybatisDemoApplication {
 
